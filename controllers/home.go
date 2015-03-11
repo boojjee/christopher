9e646c18b4c4653351e11702c8cc6f4063a0dc8f
bj <bj@bannapots-MacBook-Pro.local>
2015-03-11 18:09:17 +0700

    update

--- controllers/home.go
+++ controllers/home.go
@@ -1,23 +1,23 @@
 package controllers
 
 import (
-	"encoding/json"
+	// "encoding/json"
 	"github.com/gin-gonic/gin"
-	"io/ioutil"
-	"log"
+	// "io/ioutil"
+	// "log"
 )
 
-type mytype []map[string]string
+// type mytype []map[string]string
 
 func Home(c *gin.Context) {
-	var data mytype
-	file, err := ioutil.ReadFile("test.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = json.Unmarshal(file, &data)
-	if err != nil {
-		log.Fatal(err)
-	}
-	c.JSON(200, gin.H{"status": "OK!", "build": data})
+	// var data mytype
+	// file, err := ioutil.ReadFile("version.json")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// err = json.Unmarshal(file, &data)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	c.JSON(200, gin.H{"status": "OK!", "build": "2332"})
 }
